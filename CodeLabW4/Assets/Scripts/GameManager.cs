d2d1ae0f2dd431716dab5208a24e2e2d14690334
Siddarth <siddarthrgovindan@gmail.com>
2019-02-25 14:20:43 -0500

    Material for the cubes

---
+++ CodeLabW4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text ScoreText;
+    public Text TimerText;
+    public Text HighScoreText;
+
+    private float timer;
+    
+    private int score;
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            ScoreText.text = "Score: " + score;
+            highScore = score;
+        }
+    }
+
+    private int highScore;
+
+    public int HighScore
+    {
+        get
+        {
+            return highScore;
+        }
+        set
+        {
+            if (value > highScore)
+            {
+                highScore = value;
+                HighScoreText.text = "High Score: " + highScore;
+            }
+        }
+    }
+
+    public static GameManager instance;
+
+    void Start()
+    {
+        //SINGLETON
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer < 0)
+        {
+            _GameOver();
+        }
+            
+    }
+
+    void _GameOver()
+    {
+        //do game over things
+    }
+}
