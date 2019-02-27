59a949a5b4f14c76bc29d2007b34ae6f5925ea90
Siddarth <siddarthrgovindan@gmail.com>
2019-02-27 10:44:39 -0500

    freefall: calculates and displays range and height fallen after colliding with water

--- freefall/Assets/Scripts/GameManager.cs
+++ freefall/Assets/Scripts/GameManager.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    private GameObject Player;
 
-    public GameObject Player;
+    public Text ReportText;
     
     private Vector3 startPos;
     public Vector3 StartPos
     {
         get 
         {
-            Debug.Log(startPos);
             return startPos;  
         }
         set
@@ -21,12 +22,12 @@ public class GameManager : MonoBehaviour
             startPos = value;
         }
     }
+    
     private Vector3 endPos;
     public Vector3 EndPos
     {
         get
         {
-            Debug.Log(endPos);
             return endPos;
         }
         set
@@ -36,9 +37,34 @@ public class GameManager : MonoBehaviour
     }
 
     private float range;
-    private float height;
+    public float Range
+    {
+        get
+        {
+            return range;
+        }
+        set
+        {
+            range = value;
+            Debug.Log("Range: "+range);
+        }
+    }
 
-    
+    private float height;
+    public float Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            height = value;
+            Debug.Log("Height: "+height);
+        }
+    }
+
+
     void Start()
     {
         // SINGLETON
@@ -52,7 +78,7 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         
-        //
+        // Find Player object
         Player = GameObject.Find("Player");
         StartPos = Player.transform.position;
     }
@@ -62,9 +88,10 @@ public class GameManager : MonoBehaviour
     public void HitWater(Transform end)
     {
         EndPos = end.position;
-        range = Mathf.Sqrt((endPos.x-startPos.x)*(endPos.x-startPos.x))+((endPos.z-startPos.z)*(endPos.z-startPos.z));
-        //height
-        Debug.Log("Range: "+range);
+        Range = Mathf.Sqrt((EndPos.x-StartPos.x)*(EndPos.x-StartPos.x)+(EndPos.z-StartPos.z)*(EndPos.z-StartPos.z));
+        Height = Mathf.Abs(EndPos.y-StartPos.y);
+        ReportText.text = "You fell " + Height + "m and leapt across " + Range + "m.";
+       
         // reset game?
     }
 }
