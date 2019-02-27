58d201f2d014458631817955428ac6af1731628f
Siddarth <siddarthrgovindan@gmail.com>
2019-02-27 00:10:38 -0500

    freefall: saving work before stopping for now

---
+++ freefall/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float sensitivityRoll = 100f;
+    public float sensitivityYaw = 100f;
+    public float sensitivityPitch = 100f;
+    void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis( Time.deltaTime * sensitivityRoll * Input.GetAxis("Vertical"), Vector3.forward );
+        transform.rotation *= Quaternion.AngleAxis( Time.deltaTime * sensitivityYaw * Input.GetAxis("Mouse X"), Vector3.up );
+        transform.rotation *= Quaternion.AngleAxis( Time.deltaTime * sensitivityPitch * Input.GetAxis("Mouse Y"), Vector3.left );
+        //Debug.Log("Camera rotation: "+transform.rotation);
+    }
+}
---
+++ freefall/Assets/Water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    // When the player hits the water
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameManager.instance.HitWater(other.transform);
+        }
+    }
+}
