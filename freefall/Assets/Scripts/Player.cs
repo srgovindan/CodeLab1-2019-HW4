620814abdb61fa6d0cfb1629916a365857113415
Siddarth <siddarthrgovindan@gmail.com>
2019-02-26 23:03:23 -0500

    Imported First Person Drifter by Ben Esposito Imported material assets for cliff terrain Created a fresh FPS controller and Player.cs to control it

---
+++ freefall/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //Keycodes
+    public KeyCode UpKey;
+    public KeyCode DownKey;
+    public KeyCode LeftKey;
+    public KeyCode RightKey;
+    public KeyCode RunKey;
+
+    private float moveSpeed;
+    private float walkSpeed=0.1f;
+    private float runSpeed=0.3f;
+    
+    
+    private Rigidbody rb;
+    
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Move()
+    {
+        //check if walk or run
+        if (Input.GetKey(RunKey)){moveSpeed = runSpeed;}
+        else{moveSpeed = walkSpeed;}
+        
+        //move on input
+        Vector3 newTransform = new Vector3();
+        if (Input.GetKey(UpKey))
+        {
+            newTransform+=Vector3.forward*moveSpeed;
+        }
+        if (Input.GetKey(DownKey))
+        {
+            newTransform-=Vector3.forward*moveSpeed;
+        }       
+        if (Input.GetKey(LeftKey))
+        {
+            newTransform+=Vector3.left*moveSpeed;
+        }       
+        if (Input.GetKey(RightKey))
+        {
+            newTransform-=Vector3.left*moveSpeed;
+        }
+        transform.position += newTransform;
+    }
+    
+}
---
+++ freefall/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+	public ParticleSystem Ripples;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		// Turn particle effects on/off when primary mouse is clicked
+		if (Input.GetMouseButtonDown(0))
+		{
+			//Debug.Log("Primary mouse was clicked.");
+			//Ripples.Play();
+
+		}
+	}
+}
